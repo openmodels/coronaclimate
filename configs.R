341bdb6f3452070bbfa5d71366005d93b1857a09
James Rising <jarising@gmail.com>
2020-09-23 16:10:23 +0100

    new model

--- configs.R
+++ configs.R
@@ -1,7 +1,10 @@
-weather.delayA <- c(4, 8)
-weather.delayB <- c(11, 19)
-weather.subset <- c("absh", "r", "t2m", "tp")
+## head
 RESULTPATH <- '~/Dropbox/Coronavirus and Climate/results/'
 FIGUREPATH <- '~/Dropbox/Coronavirus and Climate/figures/'
 CASESPATH <- '~/Dropbox/Coronavirus and Climate/cases/'
 MOBILITYPATH <- '~/Dropbox/Coronavirus and Climate/mobility/'
+## body
+weather.delayA <- c(4, 8)
+weather.delayB <- c(11, 19)
+weather.subset <- c("q", "q2", "r", "r2", "ssrd", "ssrd2", "t2m", "t2m2", "tp", "tp2", "utci", "utci2")
+weather.subset.stagetwo <- c("q", "q2", "r", "r2", "ssrd", "ssrd2", "t2m", "t2m2", "tp", "tp2", "utci", "utci2")
